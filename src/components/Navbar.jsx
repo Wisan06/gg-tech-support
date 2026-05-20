258dba6985ee08790a711ba811c5b1eb87f09c69
Wisan06 <wgodoygomez@gmail.com>
2026-05-20 17:23:47 -0500

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,52 @@
+import { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
 import logo from '../assets/logo.png'
+
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
+
     <header className="navbar">
 
       <div className="logo">
-        <img 
-        src={logo}
-        alt="GG Tech Support"
+
+        <img
+          src={logo}
+          alt="GG Tech Support"
         />
-        
+
       </div>
 
-      <nav>
-        <a href="#services">Servicios</a>
-        <a href="#about">Nosotros</a>
-        <a href="#contact">Contacto</a>
+      <nav className={menuOpen ? 'nav active' : 'nav'}>
+
+        <a href="#services">
+          Servicios
+        </a>
+
+        <a href="#about">
+          Nosotros
+        </a>
+
+        <a href="#contact">
+          Contacto
+        </a>
+
       </nav>
 
+      <div
+        className="menu-toggle"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+
+        {menuOpen ? <FaTimes /> : <FaBars />}
+
+      </div>
+
     </header>
+
   )
 }
 
