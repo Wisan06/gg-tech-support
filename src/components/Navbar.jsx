2f49bfe990d58483b388ebe5d52d349e630b1706
Wisan06 <wgodoygomez@gmail.com>
2026-05-20 14:09:41 -0500

    Inicio proyecto GG Tech Support

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <header className="navbar">
+
+      <div className="logo">
+        GG Tech Support
+      </div>
+
+      <nav>
+        <a href="#services">Servicios</a>
+        <a href="#about">Nosotros</a>
+        <a href="#contact">Contacto</a>
+      </nav>
+
+    </header>
+  )
+}
+
+export default Navbar
